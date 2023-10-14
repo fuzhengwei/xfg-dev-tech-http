ee3353c0790ced3eb3aafa52c6f75f51ba98d7d5
小傅哥 <184172133@qq.com>
2023-10-14 16:57:38 +0800

    feat：应用入口

--- xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/Application.java
+++ xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/Application.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Configurable
-@EnableDiscoveryClient
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
