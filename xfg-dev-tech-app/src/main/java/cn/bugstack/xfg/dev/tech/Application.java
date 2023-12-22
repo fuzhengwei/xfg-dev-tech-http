64d3c27929d90d12fdee3003c784ff9b0345297f
小傅哥 <184172133@qq.com>
2023-12-22 22:08:07 +0800

    feat：去掉不需要的注释

--- xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/Application.java
+++ xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/Application.java
@@ -3,7 +3,6 @@ package cn.bugstack.xfg.dev.tech;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
